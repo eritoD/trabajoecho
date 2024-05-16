1364629403d6f0bd1972d3b357d476439965078f
eritoD <eri.diaz@duocuc.cl>
2024-05-16 19:38:37 -0400

    sisis

--- jspruebas.js
+++ jspruebas.js
@@ -1,26 +1,35 @@
-
-
 const settings = {
-     async: true,
-     crossDomain: true,
-     url: 'https://yahoo-weather5.p.rapidapi.com/weather?location=punta%20arena&format=json&u=c',
-     method: 'GET',
-     headers: {
-         'X-RapidAPI-Key': 'c93daf93b2msh09a39a66c8e3902p1dbdcfjsn0a02a09d3934',
-         'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
-     }
- };
+	async: true,
+	crossDomain: true,
+	url: 'https://pokemon-go1.p.rapidapi.com/type_effectiveness.json',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c93daf93b2msh09a39a66c8e3902p1dbdcfjsn0a02a09d3934',
+		'X-RapidAPI-Host': 'pokemon-go1.p.rapidapi.com'
+	}
+};
 
- $.ajax(settings).done(function (response) {
-     console.log(response[0]);
-     $("#cosas").ready(response)
+$.ajax(settings).done(function (response) {
+	console.log(response);
 
-     $.each(response, function(i,item){
-        $("#cosas").append(item.current_observation);
-     });
+    $.each(response, function(i,item){
+        $("#cosas").append(    
+        '<div class="swiper-slide">' +
+                '<div class="product">' +
+                    '<div class="product-img">' +
+                        '<h4>ansklcnailcni</h4>' +
+                        '<img src="images/food1.png" alt="">' +
+                    '</div>' +
+                    '<div class="product-txt">' +
+                        '<h4>producto</h4>' +
+                        '<p>calidad premium</p>' +
+                        '<span class="price">$80.00</span>' +
+                    '</div>' +
+                '</div>' +
+            '</div>'
+        );
+    });
 
-
-
- });
+});
 
 
